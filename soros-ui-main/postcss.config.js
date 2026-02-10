9739fca875a4dc7eed64f8f4586db2b360bca5fe
DhanvardiniRajendran25 <dhanvardini.r@gmail.com>
2026-02-09 19:00:57 -0500

    Remove all files to restructure project

--- soros-ui-main/postcss.config.js
+++
@@ -1,7 +0,0 @@
-// Using ESM export default as expected by Vite
-export default {
-    plugins: {
-        tailwindcss: {}, // Standard key for Tailwind plugin
-        autoprefixer: {}, // Standard key for Autoprefixer plugin
-    },
-}
